43272b103444a6c046c0921d7681b038e6138d0e
DelsinC6806 <76930010+DelsinC6806@users.noreply.github.com>
2021-01-08 14:31:09 +0800

    Added Canvas

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public float time;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void countDown()
+    {
+
+    }
+}
