e51fe880b4817d2b265cda58af6d407a844fd125
DelsinC6806 <76930010+DelsinC6806@users.noreply.github.com>
2021-01-11 13:19:35 +0800

    Basic item material,Drag&Drop,ItemSpawning,

---
+++ Assets/Script/DragNDrop.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class DragNDrop : MonoBehaviour
+{
+    GameObject target;
+    bool isMouseDragging;
+    bool isDragUp = false;
+    Vector3 screenPosition;
+    Vector3 offset;
+
+    void Update()
+    {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hitInfo;
+                target = ReturnClickedObject(out hitInfo);
+                if (target.transform.tag == "draggable")
+                {
+                    isMouseDragging = true;
+
+                screenPosition = Camera.main.WorldToScreenPoint(target.transform.position);
+                offset = target.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z));
+                }
+            }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+        isMouseDragging = false;
+        isDragUp = false;
+        }
+
+        if (isMouseDragging)
+        {
+            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z);
+            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) + offset;
+            target.transform.position = currentPosition;
+        }
+    }
+
+
+    GameObject ReturnClickedObject(out RaycastHit hit)
+    {
+        GameObject targetObject = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            targetObject = hit.collider.gameObject;
+        }
+        return targetObject;
+    }
+}
--- Assets/Script/ItemSpawningScript.cs
+++ Assets/Script/ItemSpawningScript.cs
@@ -6,14 +6,25 @@ public class ItemSpawningScript : MonoBehaviour
 {
     public Transform spawnPoint;
     public GameObject[] items;
-
-    void Start()
-    {
-        
-    }
+    int count = 0 ;
 
     void Update()
     {
-        
+        Invoke("Spawn", 0.2f);
+    }
+
+    void Spawn()
+    {
+        if(count < 50)
+        {
+            int index = Random.Range(0,items.Length-1);
+            Instantiate(items[index], spawnPoint.position,Quaternion.identity);
+            Instantiate(items[index], spawnPoint.position,Quaternion.identity);
+            Vector3 newSpawnPoint = spawnPoint.position;
+            newSpawnPoint.y += 2;
+            spawnPoint.position = newSpawnPoint;
+            count += 2;
+            Debug.Log(count);
+        }
     }
 }
